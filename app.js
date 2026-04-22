37dd398b8462418cdd96aadb71cfbd249ef0d3c1
saikrishna_vm <saikrishnaprasad15@gmail.com>
2026-04-22 20:18:41 +0530

    changed app

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const http = require('http');
-http.createServer((req,res)=>{
-  res.end("Hello DevOps");
-}).listen(3000);
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('CI/CD Working 🚀');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
